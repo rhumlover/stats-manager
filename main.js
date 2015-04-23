fd38c11af0597f1fd633c8012551f8b1772a6672
Thomas Le Jeune <thomas.lejeune@dailymotion.com>
2015-04-23 14:06:56 -0700

    Add Gemius Plugin

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var StatsManager = require('./src/stats-manager.coffee');
 
 StatsManager.plugins = {
     ComScorePlugin: require('./src/plugins/comscore.coffee'),
+    GemiusPlugin: require('./src/plugins/gemius.coffee'),
     GoogleAnalyticsPlugin: require('./src/plugins/googleanalytics.coffee'),
     SiteCatalystPlugin: require('./src/plugins/sitecatalyst.coffee')
 }
